5ca345c80c9a6e0c5e36281770fa8f80603406cf
hoanglong7822 <vthl2k2@gmail.com>
2025-02-17 20:55:10 +0700

    update component DiscountHotels

--- src/routes/home/components/slider-hotels/DiscountHotels.jsx
+++ src/routes/home/components/slider-hotels/DiscountHotels.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart as regularHeart } from '@fortawesome/free-regular-svg-icons';
-import { faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons';
+import {
+  faAlignLeft,
+  faArrowLeft,
+  faArrowRight,
+  faHeart as solidHeart,
+} from '@fortawesome/free-solid-svg-icons';
 import { store } from '../../../../redux/store';
 import { addFavoriteHotel } from '../../../../redux/actions';
 import { useNavigate } from 'react-router-dom';
-
 const DiscountHotels = ({ DiscountHotelsData }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const favorite = useSelector((state) => state.favorite);
@@ -16,8 +20,6 @@ const DiscountHotels = ({ DiscountHotelsData }) => {
     ...hotel,
     isFavorite: favorite.some((fav) => fav.id === hotel.id),
   }));
-
-  const visibleHotels = 4;
 
   const dispatchFavorite = (hotel) => {
     store.dispatch(addFavoriteHotel(hotel));
@@ -34,7 +36,23 @@ const DiscountHotels = ({ DiscountHotelsData }) => {
       prev >= hotelsWithFavoriteStatus.length - 1 ? 0 : prev + 1
     );
   };
+  const [visibleHotels, setVisibleHotels] = useState(5);
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setVisibleHotels(2);
+      } else {
+        setVisibleHotels(5);
+      }
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
     <div className="px-2 md:px-0 container mx-auto mt-10">
       <div className="flex items-center justify-between mb-6">
@@ -47,15 +65,15 @@ const DiscountHotels = ({ DiscountHotelsData }) => {
         {currentIndex > 0 && (
           <button
             onClick={prevSlide}
-            className="absolute left-0 top-1/2 transform -translate-y-1/2 p-2 bg-slate-100 text-slate-700 rounded-full hover:bg-opacity-75 focus:outline-none z-10 w-10 h-10 flex items-center justify-center"
+            className="absolute left-0 top-1/2 transform -translate-y-1/2 -translate-x-1/4 p-2 bg-slate-50 text-slate-700 rounded-full hover:bg-opacity-75 focus:outline-none z-10 w-10 h-10 flex items-center justify-center"
           >
-            &lt;
+            <FontAwesomeIcon icon={faArrowLeft} />
           </button>
         )}
 
-        <div className="flex overflow-hidden">
+        <div className="overflow-hidden">
           <div
-            className="flex w-full gap-2 transition-transform duration-500 ease-in-out bg-blue-600"
+            className="flex w-full transition-transform duration-500 ease-in-out overflow-hidden"
             style={{
               transform: `translateX(-${currentIndex * (100 / visibleHotels)}%)`,
             }}
@@ -63,9 +81,9 @@ const DiscountHotels = ({ DiscountHotelsData }) => {
             {hotelsWithFavoriteStatus.map((hotel) => (
               <div
                 key={hotel.Id}
-                className="w-full bg-black md:w-1/2  transition-transform duration-300 transform hover:scale-105 relative"
+                className="w-1/2 shrink-0 md:w-1/5  transition-transform duration-300 transform hover:scale-105 relative p-2"
               >
-                <div className="bg-white shadow-md rounded-lg overflow-hidden">
+                <div className="bg-white h-full shadow-md rounded-lg overflow-hidden">
                   <img
                     src={hotel.images}
                     alt={hotel.name}
@@ -76,7 +94,7 @@ const DiscountHotels = ({ DiscountHotelsData }) => {
                     onClick={() => {
                       dispatchFavorite(hotel);
                     }}
-                    className="absolute top-4 right-4 items-center pt-1 w-8 h-8 rounded-full bg-white shadow-md focus:outline-none"
+                    className="absolute  top-4 right-4 items-center pt-1 w-8 h-8 rounded-full bg-white shadow-md focus:outline-none"
                   >
                     <FontAwesomeIcon
                       icon={hotel.isFavorite ? solidHeart : regularHeart}
@@ -118,9 +136,9 @@ const DiscountHotels = ({ DiscountHotelsData }) => {
         {currentIndex + visibleHotels < hotelsWithFavoriteStatus.length && (
           <button
             onClick={nextSlide}
-            className="absolute right-0 top-1/2 transform -translate-y-1/2 p-2 bg-slate-100 text-slate-700 rounded-full hover:bg-opacity-75 focus:outline-none z-10 w-10 h-10 flex items-center justify-center"
+            className="absolute right-0 top-1/2 transform -translate-y-1/2 translate-x-1/4 p-2 bg-slate-50 text-slate-700 rounded-full hover:bg-opacity-75 focus:outline-none z-10 w-10 h-10 flex items-center justify-center"
           >
-            &gt;
+            <FontAwesomeIcon icon={faArrowRight} />
           </button>
         )}
       </div>
